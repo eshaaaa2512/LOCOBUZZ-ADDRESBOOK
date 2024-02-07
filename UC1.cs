9fc6d0dc2b18c482c5bd28a68e00940c7578eb40
Esha <eshadeswal@gmail.com>
2024-02-07 15:40:25 +0530

    [Esha]Add: Entering the Values in AddressBook

--- UC1.cs
+++ UC1.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class AddressBook
+namespace AddressBook
 {
     class Contacts
     {
@@ -15,12 +15,70 @@ class AddressBook
         private string Email;
 
     }
-    static void Main(string[] args)
+    class AddressBook
     {
-        Console.WriteLine("Welcome AddressBook");
-
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
     }
+    class UC2
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome AddressBook");
+            
+            Console.WriteLine("Enter the First_Name: ");
+            AddressBook contact = new AddressBook();
+            contact.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Last_Name: ");
+            contact.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Address: ");
+            contact.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter the City: ");
+            contact.City = Console.ReadLine();
+
+            Console.WriteLine("Enter the State: ");
+            contact.State = Console.ReadLine();
+
+            Console.WriteLine("Enter the PostalCode: ");
+            contact.PostalCode = Console.ReadLine();
+
+            Console.WriteLine("Enter the PhoneNumber: ");
+            contact.PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter the Email: ");
+            contact.Email = Console.ReadLine();
+        }
+        
+        
+        
+        
+    }
+
 }
+            
+
+
+
+        
+       
+
+        
+    
+  
+
+
+
 
         
 
