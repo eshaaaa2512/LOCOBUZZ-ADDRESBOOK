a5d2681ae04bb9bf3f7218758422ba3d3002503f
Esha <eshadeswal@gmail.com>
2024-02-07 15:04:18 +0530

    [Esha] Add: AddressBook

---
+++ UC1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class AddressBook
+{
+    class Contacts
+    {
+        private string First_Name;
+        private string Last_Name;
+        private string Address;
+        private string City;
+        private string state;
+        private string PostalCode;
+        private string phoneNumber;
+        private string Email;
+
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome AddressBook");
+
+    }
+}
+
+        
+
+
+
+    
+    
+
+
+
+
+
+
+
